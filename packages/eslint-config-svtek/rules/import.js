bd438b8932c4c6bb8cc8a5d525098303bc7e0110
Alican Çubukçuoğlu <alicancubukcuoglu@gmail.com>
2018-06-13 00:02:20 -0700

    Configure "import/order"

--- packages/eslint-config-svtek/rules/import.js
+++ packages/eslint-config-svtek/rules/import.js
@@ -2,6 +2,13 @@ module.exports = {
   rules: {
     // We have a better ordering strategy
     'import/first': 0,
+    'import/order': [
+      'error',
+      {
+        groups: [['builtin', 'external', 'internal'], ['parent'], ['sibling', 'index']],
+        'newlines-between': 'always',
+      },
+    ],
 
     // We have to do require() for platform specific imports and that triggers this rule
     'import/newline-after-import': 0,
