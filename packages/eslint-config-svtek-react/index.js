b3e5461945573af2e69d18311ffcd438098228ed
omerfarukoner <farukoner61@gmail.com>
2022-01-20 18:21:25 +0300

    v0.6.0

--- packages/eslint-config-svtek-react/index.js
+++ packages/eslint-config-svtek-react/index.js
@@ -1,9 +1,9 @@
 module.exports = {
   extends: [
     'eslint-config-airbnb',
-    'eslint-config-svtek/rules/base.js',
-    'eslint-config-svtek/rules/flow.js',
-    'eslint-config-svtek/rules/import.js',
+    '../eslint-config-svtek/rules/base.js',
+    '../eslint-config-svtek/rules/flow.js',
+    '../eslint-config-svtek/rules/import.js',
     './rules/react.js',
   ],
 };
